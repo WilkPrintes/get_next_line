517d4b1927d550b22b5da4030efafe0bb08e65f9
WilkPrintes <wilkp90@gmail.com>
2021-11-03 23:49:31 -0300

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 10:56:53 by wprintes          #+#    #+#             */
-/*   Updated: 2021/11/03 23:39:48 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/03 23:48:06 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,31 +57,26 @@ size_t	find_n(char *buffer)
 char	*read_line(char *buffer, int buffer_size, int fd)
 {
 	ssize_t size;
+	ssize_t total;
 	char *temp;
 	char *result;
-	static char *backup;
 
+	total = 0;
 	temp = malloc(sizeof(char) * 9999);
 	temp = ft_strdup(buffer);
-	if (backup != NULL)
-	{
-		temp = ft_strdup(backup);
-		backup = NULL;
-	}
 	buffer = malloc(sizeof(char) * buffer_size);
 	while(n_exists(buffer) != 1 && size > 0)
 	{
 		size = read (fd, buffer, buffer_size);
 		temp = ft_strjoin(temp, buffer);
+		total = total + size;
 	}
+	printf("len %ld || total %ld\n", ft_strlen(temp), total);
 	free(buffer);
 	result = malloc(sizeof(char) * find_n(temp) + 1);
 	ft_memmove(result, temp, find_n(temp));
 	if (size != 0)
-	{
-		backup = temp + find_n(temp) + 1;
 		result[find_n(temp)] = '\n';
-	}
 	free(temp);
 	return (result);
 }
