948957b704e0a32bfe1421c432013fffd566d6c3
WilkPrintes <wilkp90@gmail.com>
2021-11-03 23:40:07 -0300

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:27:15 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/31 14:57:52 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/03 21:53:21 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+void bom_dia(int *a);
 
 int	main(void)
 {
