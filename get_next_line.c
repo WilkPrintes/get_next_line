047a1e132e593354887bfbcae1299cbe804aec39
WilkPrintes <wilkp90@gmail.com>
2021-11-08 17:23:43 -0300

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 10:56:53 by wprintes          #+#    #+#             */
-/*   Updated: 2021/11/08 16:57:00 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/08 17:23:12 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,10 +104,11 @@ char	*read_line(char *buffer, ssize_t buffer_size, int fd, ssize_t size, char **
 	free(buffer);
 	result = malloc(sizeof(char) * (find_n(temp) + 1));
 	ft_memmove(result, temp, find_n(temp));
-	if (size != 0)
+	if (size == buffer_size)
 	{
 		result[find_n(temp)] = '\n';
-		*backup = ft_strdup(temp + find_n(temp) + 1);
+		if (temp[find_n(temp) + 1] != '\0')
+			*backup = ft_strdup(temp + find_n(temp) + 1);
 	}
 	result[find_n(temp) + 1] = '\0';
 	free(temp);
