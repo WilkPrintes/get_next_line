0112808ae74bd3e1080ba32b81fed6bb8e0d6f77
WilkPrintes <wilkp90@gmail.com>
2021-11-14 11:54:52 -0300

    sad

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:27:05 by wprintes          #+#    #+#             */
-/*   Updated: 2021/11/07 22:47:42 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/14 11:54:34 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(const char *src);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int c);
+char	*join(char *s1_val, char *s2_val, char *result);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+ssize_t	ft_strlen(const char *s);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *src);
 
-#endif  
+#endif
