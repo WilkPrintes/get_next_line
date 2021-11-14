fe8a8f2271757c57884a3ff530d5e05f53ca97ea
WilkPrintes <wilkp90@gmail.com>
2021-11-13 21:48:16 -0300

    new condition for backup

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 10:56:53 by wprintes          #+#    #+#             */
-/*   Updated: 2021/11/13 10:47:56 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/13 21:47:53 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,8 +115,18 @@ char	*read_line(char *buffer, int fd, ssize_t size, char **backup)
 	free(buffer);
 	result = malloc(sizeof(char) * (find_n(temp) + 1));
 	ft_memmove(result, temp, find_n(temp) + 1);
-	if ((total > ft_strlen(result) && BUFFER_SIZE != 1) || ((total == BUFFER_SIZE) && BUFFER_SIZE != 1))
-		*backup = ft_strdup(temp + find_n(temp) + 1);
+	if (total == BUFFER_SIZE)
+	{
+		buffer = malloc(sizeof(char) * BUFFER_SIZE));
+		size = read (fd, buffer, BUFFER_SIZE);
+		if (size != 0)
+			*backup = buffer;
+	}
+	if (BUFFER_SIZE != 1 && backup)
+	{
+		if (total > ft_strlen(result))
+			*backup = ft_strdup(temp + find_n(temp) + 1);
+	}
 	result[find_n(temp) + 1] = '\0';
 	free(temp);
 	return (result);
