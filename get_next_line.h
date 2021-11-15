3b7e9be3bfdbfff25b97d8b6fa766a4bc6cccd41
WilkPrintes <wilkp90@gmail.com>
2021-11-15 00:23:53 -0300

    update functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 11:27:05 by wprintes          #+#    #+#             */
-/*   Updated: 2021/11/14 23:36:47 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/11/15 00:19:15 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 ssize_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-ssize_t	find_n(char *buffer);
+size_t	find_n(char *buffer);
 
 #endif
